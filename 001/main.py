8b54b39e2b6fda4b9609b6bbff7af57432f180c5
vil1240 <vil1240@thi.de>
2021-03-18 18:59:13 +0100

    First init

---
+++ 001/main.py
@@ -0,0 +1,17 @@
+# a = float(input("Erster Wert: "))
+# b = float(input("Zweiter Wert: "))
+#
+# if(a > b) or (a < b):
+#     print("a ungleich b")
+# else:
+#     print("a gleich b")
+
+Wort = "HALLO"
+
+for i in range(10):
+    print(i)
+
+
+x = bin(1023)
+
+print(x)
